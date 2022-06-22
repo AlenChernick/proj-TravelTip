6f429803b71b36a53b61ec8d9e59be574e3251c3
AlenChernick <crazyguy771@gmail.com>
2022-06-22 17:46:07 +0300

    added key

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -23,36 +23,36 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
 }
 
 function addMarker(loc) {
-      // Create the initial InfoWindow.
-  let infoWindow = new google.maps.InfoWindow({
-    content: "Click the map to set marker!",
-    position: loc,
-  });
-
-  infoWindow.open(gMap);
-
-  // Configure the click listener.
-  gMap.addListener("click", (mapsMouseEvent) => {
-    // Close the current InfoWindow.
-    infoWindow.close();
-
-    // Create a new InfoWindow.
-    infoWindow = new google.maps.InfoWindow({
-      position: mapsMouseEvent.latLng,
-      
+    // Create the initial InfoWindow.
+    let infoWindow = new google.maps.InfoWindow({
+        content: "Click the map to set marker!",
+        position: loc,
     });
-    infoWindow.setContent(
-      JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
-    );
+
     infoWindow.open(gMap);
-    var marker = new google.maps.Marker({
-        position: mapsMouseEvent.latLng,
-        map: gMap,
-        title: 'Dont tuch me!!',
-        // icon: '😎',
-                
+
+    // Configure the click listener.
+    gMap.addListener("click", (mapsMouseEvent) => {
+        // Close the current InfoWindow.
+        infoWindow.close();
+
+        // Create a new InfoWindow.
+        infoWindow = new google.maps.InfoWindow({
+            position: mapsMouseEvent.latLng,
+
+        });
+        infoWindow.setContent(
+            JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+        );
+        infoWindow.open(gMap);
+        var marker = new google.maps.Marker({
+            position: mapsMouseEvent.latLng,
+            map: gMap,
+            title: 'Dont tuch me!!',
+            // icon: '😎',
+
+        });
     });
-  });
     // return marker;
 }
 
@@ -65,7 +65,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyDfcnHbNhQIaEJqLmNV9jnO9zN43n_euTw'; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
